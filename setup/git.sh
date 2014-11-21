69c22f59fe1a1fa1c48e618b177cc5301389cbad
Rikk Hill <rikk@unashamedly.co.uk>
2014-11-21 20:52:45 +0000

    Git push behaviour (test with this commit)

--- setup/git.sh
+++ setup/git.sh
@@ -5,6 +5,11 @@
 git config --global user.name "Rikk Hill"
 git config --global user.email "rikk@unashamedly.co.uk"
 
+# Pushing
+
+git config --global push.default simple
+
+# Aliases
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.st status
