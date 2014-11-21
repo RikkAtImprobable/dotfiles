5557d8ecf2e6f12a2603dd9e95ab6bd0d107e4ea
Rikk Hill <rikk@unashamedly.co.uk>
2014-11-21 20:46:19 +0000

    bashrc and basic setup

---
+++ setup/git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Global git config commands that are run once at setup
+
+# User
+git config --global user.name "Rikk Hill"
+git config --global user.email "rikk@unashamedly.co.uk"
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.st status
---
+++ setup/tamper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Tamper with existing files appropriately
+
+# .bashrc
+# Inspect .bashrc for previous tampering
+# Append accordingly if it's not been tampered with
+
+if ! grep -q "# tampered with by .dotfiles" ~/.bashrc ; then
+    echo "source ~/.dotfiles/bashrc/* # tampered with by .dotfiles" >> ~/.bashrc
+fi
+
+# TODO link dotfiles in ~/
